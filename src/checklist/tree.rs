9d4f00f6d1b06495e3d5ee4bc19083a7d2253054
Erik Hollensbe <git@hollensbe.org>
2024-01-16 18:16:16 -0800

    properly calculate the position from the top
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/checklist/tree.rs
+++ src/checklist/tree.rs
@@ -9,6 +9,21 @@ pub struct ChecklistTree {
 
 impl ChecklistTree {
     fn position_from_top(&self) -> usize {
-        self.current.overlap(&self.first_visible)
+        let mut total = 0;
+
+        let cur = self.current.clone();
+        for (x, pos) in cur.iter().rev().enumerate() {
+            let mut pos = pos.clone();
+            let first_pos = self.first_visible[cur.len() - x];
+            while first_pos != pos && pos > 0 {
+                pos -= 1;
+                total += 1;
+            }
+            if first_pos != pos {
+                total += 1;
+            }
+        }
+
+        total
     }
 }
